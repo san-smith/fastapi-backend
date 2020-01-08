45d01bf294189f93963337d0b0eaad9a56c8c2e0
san-smith <san-smith@mail.ru>
2020-01-08 20:08:27 +0700

    Add user model

--- app/db/events.py
+++ app/db/events.py
@@ -3,18 +3,28 @@ from fastapi import FastAPI
 from loguru import logger
 
 from app.core.config import DATABASE_URL, MAX_CONNECTIONS_COUNT, MIN_CONNECTIONS_COUNT
+from time import sleep
 
 
 async def connect_to_db(app: FastAPI) -> None:
     logger.info("Connecting to {0}", repr(DATABASE_URL))
+    count = 0
 
-    app.state.pool = await asyncpg.create_pool(
-        str(DATABASE_URL),
-        min_size=MIN_CONNECTIONS_COUNT,
-        max_size=MAX_CONNECTIONS_COUNT,
-    )
+    while count < 2:
+        try:
+            logger.info("try connecting... {0}", count)
+            app.state.pool = await asyncpg.create_pool(
+                str(DATABASE_URL),
+                min_size=MIN_CONNECTIONS_COUNT,
+                max_size=MAX_CONNECTIONS_COUNT,
+            )
 
-    logger.info("Connection established")
+            logger.info("Connection established")
+            break
+        except:
+            count += 1
+            sleep(2)
+
 
 
 async def close_db_connection(app: FastAPI) -> None:
